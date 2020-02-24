34196039590e8805255106fde5946fa24de216e5
RoskiDeluge <roberto.delgado@gmail.com>
2020-02-23 18:01:55 -1000

    Stage 1 completed - BubblePage rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import BubblePage from "./components/BubblePage";
