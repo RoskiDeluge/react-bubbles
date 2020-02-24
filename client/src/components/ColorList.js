24ee9532265de888fc9775a632fd541d83b96627
RoskiDeluge <roberto.delgado@gmail.com>
2020-02-23 19:08:42 -1000

    saveEdit and deleteColor functions are working, but colors.map is not rendering pro

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,10 +21,30 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+          updateColors(res.data);
+          // setMovie(initialMovie);
+          console.log(res.data);
+          // props.history.push("/");
+      })
+      .catch(err => {
+          console.log(err);
+      })
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    // e.preventDefault();
+    axiosWithAuth()
+      .delete(`/colors/${color.id}`)
+      .then(res => {
+        updateColors(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
   };
 
   return (
